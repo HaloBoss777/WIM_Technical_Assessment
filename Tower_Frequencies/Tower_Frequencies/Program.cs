3d1d9ce8cabb7a1c0afa3a02f25a22e4f52f11db
HaloBoss777 <dewaldlasiandra@gmail.com>
2025-01-10 20:06:42 +0200

    Created Project Template

---
+++ Tower_Frequencies/Tower_Frequencies/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+
+/*
+ * Project By Dewald Oosthuizen for WIM Technologies
+ * 
+ * Main Purpose of this project is to create a program that can effectively assign frequency signals to Cell Phone Towers to minimize potential interferince between towers.
+ * 
+ * Program: Console Application.
+ * Cell Tower info: Read from chosen file.
+ * Frequency Range: 
+ * 
+*/
+
+
+namespace Tower_Frequencies
+{
+        
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+}
